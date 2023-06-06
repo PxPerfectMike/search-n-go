bc881fba250f8a5f3e351e95458c5e721b31dd13
pxperfectmike <pxperfectmike@gmail.com>
2023-06-06 13:15:00 -0700

    update rollup config file to use umd format

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
 export default {
-    input: 'src/index.js', // Replace 'src/index.js' with the entry point of your project
-    output: {
-      file: 'dist/bundle.js', // Replace 'dist/bundle.js' with the desired output file path
-      format: 'cjs' // Replace 'cjs' with the desired output format (e.g., 'esm', 'umd', 'iife')
-    }
-    // Add any additional configuration options here
-  };
-  
+	input: 'src/index.js',
+	output: {
+		file: 'dist/bundle.js',
+		format: 'cjs', // Replace 'cjs' with the desired output format (e.g., 'esm', 'umd', 'iife')
+	},
+};
