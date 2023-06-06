49da51f8187bbfaa958a17fc85d221e76e39ec87
pxperfectmike <pxperfectmike@gmail.com>
2023-06-06 15:48:41 -0700

    add hashed search

--- src/hashedSearch.js
+++ src/hashedSearch.js
@@ -1,9 +1,62 @@
-// Uses Hashing
-export function hashedSearch(hashTable, target) {
-	const index = hashFunction(target);
-	return hashTable[index];
-}
+class HashTable {
+	constructor(size = 53) {
+		this.keyMap = new Array(size);
+	}
 
-function hashFunction(key) {
-	// Implement a hash function here
+	_hash(key) {
+		let total = 0;
+		let WEIRD_PRIME = 31;
+		for (let i = 0; i < Math.min(key.length, 100); i++) {
+			let char = key[i];
+			let value = char.charCodeAt(0) - 96;
+			total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+		}
+		return total;
+	}
+
+	set(key, value) {
+		let index = this._hash(key);
+		if (!this.keyMap[index]) {
+			this.keyMap[index] = [];
+		}
+		this.keyMap[index].push([key, value]);
+	}
+
+	get(key) {
+		let index = this._hash(key);
+		if (this.keyMap[index]) {
+			for (let i = 0; i < this.keyMap[index].length; i++) {
+				if (this.keyMap[index][i][0] === key) {
+					return this.keyMap[index][i][1];
+				}
+			}
+		}
+		return undefined;
+	}
+	_resize() {
+		const newSize = this.keyMap.length * 2;
+		const oldMap = this.keyMap;
+		this.keyMap = new Array(newSize);
+		this.count = 0;
+		for (const chain of oldMap) {
+			if (chain) {
+				for (const [key, value] of chain) {
+					this.set(key, value);
+				}
+			}
+		}
+	}
+
+	set(key, value) {
+		let index = this._hash(key);
+		if (!this.keyMap[index]) {
+			this.keyMap[index] = [];
+		}
+		this.keyMap[index].push([key, value]);
+		this.count++;
+
+		if (this.count / this.keyMap.length > 0.7) {
+			this._resize();
+		}
+	}
 }
