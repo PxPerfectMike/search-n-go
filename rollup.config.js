f2b03d5cc1c03dff3aa08d3928d9899799c2ada1
pxperfectmike <pxperfectmike@gmail.com>
2023-06-06 13:09:46 -0700

    add module bundler, config file, and gitignore

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+export default {
+    input: 'src/index.js', // Replace 'src/index.js' with the entry point of your project
+    output: {
+      file: 'dist/bundle.js', // Replace 'dist/bundle.js' with the desired output file path
+      format: 'cjs' // Replace 'cjs' with the desired output format (e.g., 'esm', 'umd', 'iife')
+    }
+    // Add any additional configuration options here
+  };
+  
